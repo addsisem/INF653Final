006206b2e93febadd367cef0ef992673527e6592
addsisem <addysonsisemore@gmail.com>
2023-05-11 04:58:31 +0000

    Final commit

--- controllers/statesController.js
+++ controllers/statesController.js
@@ -1,200 +1,190 @@
 const State = require('../model/States');
 const data = {
-    states: require('../model/state.json'),
-    setStates: function(data) {this.states = data}
+  states: require('../model/state.json'),
+  setStates: function(data) { this.states = data }
 }
 
 const getAllStates = async (req, res) => {
 
-    if(req.query.contig)
-    {
-        if(req.query.contig === 'true') 
-        {            
-            statesDB = await State.find();
+  if (req.query.contig) {
+    if (req.query.contig === 'true') {
+      statesDB = await State.find();
 
-            statesDB.splice(49, 1);
-            statesDB.splice(11, 1);
+      statesDB.splice(49, 1);
+      statesDB.splice(11, 1);
 
-            let allStates = statesDB.map(state => ({
-                ...data.states.find(({ code }) => state.stateCode == code),
-                ...{'funfacts' : state.funfacts},
-            }));
+      let allStates = statesDB.map(state => ({
+        ...data.states.find(({ code }) => state.stateCode == code),
+        ...{ 'funfacts': state.funfacts },
+      }));
 
-            res.json(allStates);    
+      res.json(allStates);
 
-        } else 
-        {
-            statesDB = await State.find({stateCode: "AK"});
-            stateDB = await State.find({stateCode: "HI"});
+    } else {
+      statesDB = await State.find({ stateCode: "AK" });
+      stateDB = await State.find({ stateCode: "HI" });
 
-            const contig = statesDB.concat(stateDB);
+      const contig = statesDB.concat(stateDB);
 
-            let allStates = contig.map(state => ({
-                ...data.states.find(({ code }) => state.stateCode == code),
-                ...{'funfacts' : state.funfacts},
-            }));
+      let allStates = contig.map(state => ({
+        ...data.states.find(({ code }) => state.stateCode == code),
+        ...{ 'funfacts': state.funfacts },
+      }));
 
-            res.json(allStates);
-        }
-    } else 
-    {
-        statesDB = await State.find();
-
-        let allStates = statesDB.map(state => ({
-            ...data.states.find(({ code }) => state.stateCode == code),
-            ...{'funfacts' : state.funfacts},
-        }));
-    
-        let result = data.states.map(state => ({
-            ...state,
-            ...allStates.find(({ code }) => state.code == code),
-        }));
-    
-        res.json(result);
+      res.json(allStates);
     }
-}
+  } else {
+    statesDB = await State.find();
 
-const getState = async (req, res) => {  
-    
-    const stateDB = await State.findOne({ stateCode: req.params.state}).exec();
-    const state = await data.states.find(st => st.code === req.params.state);
+    let allStates = statesDB.map(state => ({
+      ...data.states.find(({ code }) => state.stateCode == code),
+      ...{ 'funfacts': state.funfacts },
+    }));
 
-    if(!stateDB) 
-    {
-        return res.status(400).json({'message' : 'State Code parameter required.'});
-    }
+    let result = data.states.map(state => ({
+      ...state,
+      ...allStates.find(({ code }) => state.code == code),
+    }));
 
-    let result = Object.assign(state, {'funfacts' : stateDB.funfacts});
-    
     res.json(result);
+  }
 }
 
-const getCapital = async (req, res) => {  
-    
-    const state = await data.states.find(st => st.code === req.params.state);
+const getState = async (req, res) => {
 
-    if(!state) 
-    {
-        return res.status(400).json({'message' : 'State Code parameter required.'});
-    }
-    
-    res.json({'state' : state.state, 'capital' : state.capital_city});
+  const stateDB = await State.findOne({ stateCode: req.params.state }).exec();
+  const state = await data.states.find(st => st.code === req.params.state);
+
+  if (!stateDB) {
+    return res.status(400).json({ 'message': 'State Code parameter required.' });
+  }
+
+  if (stateDB.funfacts) {
+    res.json(state);
+  }
+
+  let result = Object.assign(state, { 'funfacts': stateDB.funfacts });
+
+  res.json(result);
 }
 
-const getNickname = async (req, res) => {  
-    
-    const state = await data.states.find(st => st.code === req.params.state);
+const getCapital = async (req, res) => {
 
-    if(!state) 
-    {
-        return res.status(400).json({'message' : 'State Code parameter required.'});
-    }
-    
-    res.json({'state' : state.state, 'nickname' : state.nickname});
+  const state = await data.states.find(st => st.code === req.params.state);
+
+  if (!state) {
+    return res.status(400).json({ 'message': 'State Code parameter required.' });
+  }
+
+  res.json({ 'state': state.state, 'capital': state.capital_city });
 }
 
-const getPopulation = async (req, res) => {  
-    
-    const state = await data.states.find(st => st.code === req.params.state);
+const getNickname = async (req, res) => {
 
-    if(!state) 
-    {
-        return res.status(400).json({'message' : 'State Code parameter required.'});
-    }
-    
-    res.json({'state' : state.state, 'population' : state.population});
+  const state = await data.states.find(st => st.code === req.params.state);
+
+  if (!state) {
+    return res.status(400).json({ 'message': 'State Code parameter required.' });
+  }
+
+  res.json({ 'state': state.state, 'nickname': state.nickname });
 }
 
-const getAdmission = async (req, res) => {  
-    
-    const state = await data.states.find(st => st.code === req.params.state);
+const getPopulation = async (req, res) => {
 
-    if(!state) 
-    {
-        return res.status(400).json({'message' : 'State Code parameter required.'});
-    }
-    
-    res.json({'state' : state.state, 'admitted' : state.admission_date});
+  const state = await data.states.find(st => st.code === req.params.state);
+
+  if (!state) {
+    return res.status(400).json({ 'message': 'State Code parameter required.' });
+  }
+
+  res.json({ 'state': state.state, 'population': state.population });
 }
 
-const getFunFact = async(req, res) => {
-    if(!req?.params?.state) 
-    {
-        return res.status(400).json({'message' : 'State Code is required'});
-    }
+const getAdmission = async (req, res) => {
 
-    const state = await State.findOne({ stateCode: req.params.state}).exec();
-    res.json({'funfacts' : state.funfacts[0]});
+  const state = await data.states.find(st => st.code === req.params.state);
+
+  if (!state) {
+    return res.status(400).json({ 'message': 'State Code parameter required.' });
+  }
+
+  res.json({ 'state': state.state, 'admitted': state.admission_date });
 }
 
-const addFunFact = async(req, res) => {
-    if(!req?.params?.state || !req?.body?.funfacts) 
-    {
-        return res.status(400).json({'message' : 'State Code and fun fact is required'});
-    }
+const getFunFact = async (req, res) => {
+  if (!req?.params?.state) {
+    return res.status(400).json({ 'message': 'State Code is required' });
+  }
 
-    const stateDB = await State.findOne({ stateCode: req.params.state}).exec();
+  const state = await State.findOne({ stateCode: req.params.state }).exec();
 
-    if(!stateDB)
-    {
-        return res.status(204).json({'message' : 'No state matches'});
-    }
-
-    for(var fact in req.body.funfacts)
-    {
-        stateDB.funfacts.push(req.body.funfacts[fact]);
-    }
-    
-    const result = await stateDB.save();
-    res.json(stateDB);
+  if (state) {
+    res.json({ 'funfacts': state.funfacts[0] });
+  }
 }
 
-const deleteFunFact = async (req, res) => {    
-    if(!req?.params?.state || !req?.body?.index) 
-    {
-        return res.status(400).json({'message' : 'State Code and Index is required'});
-    }
+const addFunFact = async (req, res) => {
+  if (!req?.params?.state || !req?.body?.funfacts) {
+    return res.status(400).json({ 'message': 'State Code and fun fact is required' });
+  }
 
-    const stateDB = await State.findOne({ stateCode: req.params.state}).exec();
+  const stateDB = await State.findOne({ stateCode: req.params.state }).exec();
 
-    if(!stateDB)
-    {
-        return res.status(204).json({'message' : 'No index matches'});
-    }
+  if (!stateDB) {
+    return res.status(204).json({ 'message': 'No state matches' });
+  }
 
-    stateDB.funfacts.splice(req.body.index - 1, 1);
-    const result = await stateDB.save(); 
-    
-    res.json(result);
+  for (var fact in req.body.funfacts) {
+    stateDB.funfacts.push(req.body.funfacts[fact]);
+  }
+
+  const result = await stateDB.save();
+  res.json(stateDB);
 }
 
-const updateFunFact = async (req, res) => {    
-    if(!req?.params?.state || !req?.body?.funfacts || !req?.body?.index) 
-    {
-        return res.status(400).json({'message' : 'State Code, Funfact, and Index is required'});
-    }
+const deleteFunFact = async (req, res) => {
+  if (!req?.params?.state || !req?.body?.index) {
+    return res.status(400).json({ 'message': 'State Code and Index is required' });
+  }
 
-    const stateDB = await State.findOne({ stateCode: req.params.state}).exec();
+  const stateDB = await State.findOne({ stateCode: req.params.state }).exec();
 
-    if(!stateDB)
-    {
-        return res.status(204).json({'message' : 'No index matches'});
-    }
+  if (!stateDB) {
+    return res.status(204).json({ 'message': 'No index matches' });
+  }
 
-    const result = stateDB.funfacts[req.body.index - 1] = req.body.funfacts;
-    
-    res.json(result);
+  stateDB.funfacts.splice(req.body.index - 1, 1);
+  const result = await stateDB.save();
+
+  res.json(result);
+}
+
+const updateFunFact = async (req, res) => {
+  if (!req?.params?.state || !req?.body?.funfacts || !req?.body?.index) {
+    return res.status(400).json({ 'message': 'State Code, Funfact, and Index is required' });
+  }
+
+  const stateDB = await State.findOne({ stateCode: req.params.state }).exec();
+
+  if (!stateDB) {
+    return res.status(204).json({ 'message': 'No index matches' });
+  }
+
+  const result = stateDB.funfacts[req.body.index - 1] = req.body.funfacts;
+
+  res.json(result);
 }
 
 module.exports = {
-    getAllStates,
-    getState,
-    getCapital,
-    getNickname,
-    getPopulation,
-    getAdmission,
-    getFunFact,
-    addFunFact,
-    deleteFunFact,
-    updateFunFact
+  getAllStates,
+  getState,
+  getCapital,
+  getNickname,
+  getPopulation,
+  getAdmission,
+  getFunFact,
+  addFunFact,
+  deleteFunFact,
+  updateFunFact
 }
--- server.js
+++ server.js
@@ -43,19 +43,19 @@ app.use('/', require('./routes/root'));
 app.use('/states', require('./routes/states'));
 
 app.all('*', (req, res) => {
-    res.status(404);
-    if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'));
-    } else if (req.accepts('json')) {
-        res.json({ "error": "404 Not Found" });
-    } else {
-        res.type('txt').send("404 Not Found");
-    }
+  res.status(404);
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+  } else if (req.accepts('json')) {
+    res.json({ "error": "404 Not Found" });
+  } else {
+    res.type('txt').send("404 Not Found");
+  }
 });
 
 app.use(errorHandler);
 
 mongoose.connection.once('open', () => {
-    console.log('Connected to MongoDB');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  console.log('Connected to MongoDB');
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
